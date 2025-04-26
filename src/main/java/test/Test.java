3072ea762c822e17f8e3489852a12114368f3d61
dev-jm-1024 <develop6013@gmail.com>
2025-04-27 01:59:40 +0900

    테스트 클래스를 작성하였습니다. 2025.04.27 01:48 v54

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,8 +1,15 @@
 package test;
 
+import java.util.*;
+
 public class Test {
 
     String name="";
     int age;
+
+
+    //또 수정들어감
+    private char gender;
+    private List<String> lists = new ArrayList<>();
 
 }
